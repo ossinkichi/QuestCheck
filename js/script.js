1d9a8f625d87e63a3050c19b679606abcf1ac6ca
Carlos André <loboresjunior321@gmail.com>
2025-04-12 23:22:47 -0300

    Remove frontend React application files and assets, including package.json, HTML, CSS, images, and testing files. Update JavaScript functionality for toggling between login and registration forms.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,28 @@
+const mudar = document.querySelector("#link_1");
+const voltar = document.querySelector("#link_2");
 
-let mudar  = document.querySelector('#link_1');
-let voltar = document.querySelector('#link_2');
+console.log(mudar);
 
-console.log(mudar)
+mudar.onclick = function invisible() {
+	const login = document.querySelector("#login");
+	const register = document.querySelector("#register");
+	const btn_register = document.querySelector(".register");
+	const btn_login = document.querySelector("#link_2");
 
-mudar.onclick = function invisible(){
-    
-    const login        = document.querySelector('#login');
-    const register        = document.querySelector('#register');
-    const btn_register = document.querySelector('.register');
-    const btn_login    = document.querySelector('#link_2');
+	login.style.display = "none";
+	btn_register.style.display = "none";
+	register.style.display = "block";
+	btn_login.style.display = "inline";
+};
 
+voltar.onclick = function invisible() {
+	const login = document.querySelector("#login");
+	const register = document.querySelector("#register");
+	const btn_register = document.querySelector(".register");
+	const btn_login = document.querySelector("#link_2");
 
-    login.style.display        = 'none';
-    btn_register.style.display = 'none';
-    register.style.display     = 'block';
-    btn_login.style.display    = 'inline';
-}
-
-voltar.onclick = function invisible(){
-    
-    const login        = document.querySelector('#login');
-    const register        = document.querySelector('#register');
-    const btn_register = document.querySelector('.register');
-    const btn_login    = document.querySelector('#link_2');
-
-
-    login.style.display        = 'block';
-    btn_register.style.display = 'inline';
-    register.style.display     = 'none';
-    btn_login.style.display    = 'none';
-}
+	login.style.display = "block";
+	btn_register.style.display = "inline";
+	register.style.display = "none";
+	btn_login.style.display = "none";
+};
