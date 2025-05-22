9dde3a1d991b89607659c14f620eb34ec19136aa
Ossin Kichi <loboresjunior321@gmail.com>
2025-05-22 19:28:20 -0300

    Remover arquivos de configuração obsoletos e adicionar prefixo de rota para API no AppServiceProvider

--- project/app/Providers/AppServiceProvider.php
+++ project/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Route::prefix('api')
+            ->middleware('api') // Prefixo para as rotas
+            ->group(base_path('routes/api.php')); // Carrega as rotas do arquivo api.php
     }
 }
