f3438b43f4594199023fa9e2c7601bca8325ef8d
Ossin Kichi <loboresjunior321@gmail.com>
2025-05-26 19:32:53 -0300

    Atualizar rotas para retornar a view 'home' e adicionar arquivo de view 'home.blade.php'

--- project/routes/web.php
+++ project/routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/dashboard', function () {
+    return view('home');
+});
+
+Route::get('/profile', function () {
+    return view('home');
+});
+
+Route::get('/settings', function () {
+    return view('home');
 });
