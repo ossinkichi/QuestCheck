532e97f4a5e535ae75f1032d18c76e1f2632e6be
Ossin Kichi <loboresjunior321@gmail.com>
2025-05-27 18:36:08 -0300

    Add logout icon image to project resources

--- project/resources/views/home.blade.php
+++ project/resources/views/home.blade.php
@@ -10,9 +10,13 @@
             @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-neutral-800">
-    <section class="text-amber-100">
-        <div>
-            <h1>Home</h1>
+    <section class="text-amber-100 grid-cols-2 grid place-items-center gap-4">
+        <div class="flex flex-col gap-1.5">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo Degen Tasks" class="w-48 h-48 mb-4">
+            <div class="text-wrap w-2xl">
+                <p>Caso esteja em busca de um aplicação para listar e organizar suas tarefas, seja bem vindo a degen tasks!</p>
+                <p>Venha se organizar onde você pode ganhar ganhar pontos enquanto as concluem e ainda pode troca-los por nossos incriveis itens.</p>
+            </div>
         </div>
         <div>
             <p>formulario</p>
