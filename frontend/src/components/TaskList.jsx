c99eb9a6d025a2d962f9155ee22861dd04eac7f5
Ossin Kichi <loboresjunior321@gmail.com>
2025-12-31 00:41:53 -0300

    feat: corrigir título da tarefa e ajustar estrutura da tabela de tarefas

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -3,26 +3,29 @@ import React from "react";
 const TaskList = () => {
 	const tasks = [
 		{ title: "Limpar a casa", complete: false },
-		{ title: "Limpar a cama", complete: true },
+		{ title: "Arrumar a cama", complete: true },
 		{ title: "Comprar um carro", complete: true },
 		{ title: "Pegar gold no lol", complete: false },
 	];
+
 	return (
 		<>
-			<thead>
-				<ul>
-					{tasks.map((task) => {
-						<li key={task.index}></li>;
-					})}
-				</ul>
-			</thead>
-			<tbody>
-				<ul>
-					{tasks.map((task) => (
-						<li key={task.title}>{task}</li>
+			<table>
+				<thead>
+					<tr>
+						<th>Titulo</th>
+						<li>Status</li>
+					</tr>
+				</thead>
+				<tbody>
+					{tasks.map((task, index) => (
+						<tr key={index}>
+							<td>{task.title}</td>
+							<td>{task.complete}</td>;
+						</tr>
 					))}
-				</ul>
-			</tbody>
+				</tbody>
+			</table>
 		</>
 	);
 };
