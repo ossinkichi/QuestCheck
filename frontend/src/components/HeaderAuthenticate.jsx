46959b9f9037b2c2070d191aca200f99846601f1
Ossin Kichi <loboresjunior321@gmail.com>
2026-01-03 14:16:18 -0300

    feat: adicionar botão para criar nova tarefa e atualizar dependências no package.json

--- frontend/src/components/HeaderAuthenticate.jsx
+++ frontend/src/components/HeaderAuthenticate.jsx
@@ -9,6 +9,14 @@ const HeaderAuthenticate = () => {
 			<nav className="flex gap-6">
 				<button
 					className="cursor-pointer py-2 px-6 rounded-md
+                text-base hover:bg-emerald-600 transition-all transition-discrete duration-500"
+				>
+					<a className="no-underline" href="#logout">
+						Criar nova tarefa
+					</a>
+				</button>
+				<button
+					className="cursor-pointer py-2 px-6 rounded-md
                 text-base hover:bg-rose-500 transition-all transition-discrete duration-500"
 				>
 					<a className="no-underline" href="#logout">
